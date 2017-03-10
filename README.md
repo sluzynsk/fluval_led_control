b3d33daa3667f652703d870dd90b5a9dc638910f
Steve Luzynski <steve@luzynski.net>
2017-03-09 20:58:26 -0600

    Added wunderground code to get current stats.

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# fluval_led_control
-Time of day/weather sensitive controller for Fluval Aquasky LED based on ESP8266.
+# Fluval LED control for Fluval Aquasky
+
+# Description
+
+I acquired a Fluval Aquasky LED light for my aquarium. It comes with a
+remote control with buttons for color temperature as well as for
+various time of day and weather effects.
+
+While pushing the button on the remote is cool, it seemed that it would be
+even cooler to have the lights change on their own. Out of that notion
+this application was born.
+
+The application will change the lights in the aquarium to match the current
+time of day. Optionally weather effects can be enabled; this is a configurable
+option because the lightning flashes may not appeal to you.
+
+# Installation
+
+To install the application, you will need:
+
+* The Arduino IDE
+* An ESP8266 board, such as a [NodeMCU](https://smile.amazon.com/gp/product/B010O1G1ES/ref=oh_aui_detailpage_o00_s00?ie=UTF8&psc=1).
+* One IR LED
+* One 330 ohm resistor
+* Breadboard, stripboard, or just clever soldering skills
+
+Install the application with the Arduino IDE. You will need to install
+the ESP8266 board support package through board manager, and the ESP8266
+libraries for IR, WiFi, and web server through the library manager.
+
+Note that installing a program via the Arduino IDE will blow away the Lua
+environment that comes on the module. It's possible to restore if you like;
+IMO it is a less capable solution than using the Arduino toolset anyway
+and I doubt you will miss it. 
+
+# Wiring
+
+Wiring is simple - the LED needs to be connected to GPIO0. On the module I used, that pin is labeled as D3; check the pinout of your individual module
+to be sure. Connect the resistor to the GPIO pin, the other leg of the
+resistor to the positive leg of the IR LED, and the negative leg of the LED
+to ground.
+
+# Usage
+
+On first boot, the ESP will boot up in web server mode. Locate the SSID
+for the ESP on your phone or laptop and join it.
--- fluval_led_control.ino
+++ fluval_led_control.ino
@@ -1,31 +1,79 @@
-#include <ESP8266mDNS.h>
 #include <WiFiServer.h>
 #include <WiFiClientSecure.h>
-#include <ESP8266WiFiScan.h>
 #include <WiFiUdp.h>
-#include <ESP8266WiFiGeneric.h>
-#include <ESP8266WiFiMulti.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiSTA.h>
-#include <ESP8266WiFiAP.h>
-#include <ESP8266WiFiType.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h>
 #include <IRremoteESP8266.h>
 
 #include "buttons.h"
+
+const char* ssid     = "myssid";
+const char* password = "mypassword";
+
+const char* host = "api.wunderground.com";
+const char* state = "MO";
+const char* city = "Blue_Springs";
+const char* api_key = "myapikey";
 
 IRsend irsend(0);
 
 void setup()
 {
+  Serial.begin(115200);
   irsend.begin();
+  delay(100);
+  WiFi.begin(ssid, password);
+
+ while (WiFi.status() != WL_CONNECTED) {
+   delay(500);
+   Serial.print(".");
+ }
+
+ Serial.println("");
+ Serial.println("WiFi connected");
+ Serial.println("IP address: ");
+ Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  irsend.sendNEC(orange, 32);
-  delay(2000);
-  irsend.sendNEC(blue, 32);
-  delay(2000);
-}
+  // Use WiFiClient class to create TCP connections
+    WiFiClient client;
+    const int httpPort = 80;
+    if (!client.connect(host, httpPort)) {
+      Serial.println("connection failed");
+      return;
+    }
 
+    // We now create a URI for the request
+    String url = "/api/";
+    url += api_key;
+    url += "/conditions/q/";
+    url += state;
+    url += "/";
+    url += city;
+    url += ".json";
+    Serial.print("Requesting URL: ");
+    Serial.println(url);
+
+    // This will send the request to the server
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n\r\n");
+    delay(500);
+
+    // Read all the lines of the reply from server and print them to Serial
+    while(client.available()){
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+    }
+
+    // extract the relative amount of cloudiness and lightning to use
+    // as well as the time (2 pieces of info, one API call!)
+
+
+
+    irsend.sendNEC(orange, 32);
+    delay(2000);
+    irsend.sendNEC(blue, 32);
+    delay(150000);
+}
