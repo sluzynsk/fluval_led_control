e84d051b36d53bc0da8c121b3b03d7afa83ed459
Steve <steve@luzynski.net>
2017-04-04 08:38:42 -0500

    added water temp sensor, air temp sensor, and OLED display

--- fluval_led_control.ino
+++ fluval_led_control.ino
@@ -3,12 +3,21 @@
  * for ESP8266-12E (NodeMCU) modules
  * (C)2017 by Steve Luzynski except where noted 
  * 
- * Wiring: Connect an IR LED to GPIO0 through a 330 Ohm resistor to the 3.3V 
- * power supply pin.
+ * Wiring: 
+ * 
+ * IR Control: Connect an IR LED to GPIO4 through a 330 Ohm resistor to ground.
+ * 
+ * Water Temp: Connect a waterproof DS18B20 to 3v3 & ground. Connect signal to GPIO2
+ * and pull signal up through a 120 Ohm resistor.
+ * 
+ * Inside temp: Coonect a TMP36 to A0, 3v3, and ground.
  * 
  * Codes in buttons.h reversed by pointing the OEM remote at an
  * ESP based IR decoder module, no disassembly of the LED
  * fixture was done.
+ * 
+ * Includes support for OTA firmware updates since this device
+ * will likely end up stuck under your tank someplace.
  * 
  */
 
@@ -24,19 +33,26 @@
 #include <WiFiClient.h>
 #include <IRremoteESP8266.h>
 #include <Dusk2Dawn.h>
+#include <ArduinoOTA.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <SSD1306.h>
+#include <OLEDDisplayUi.h>
+#include "SSD1306Brzo.h"
+#include "SH1106Brzo.h"
 
 #include "buttons.h"
-
-#define WEATHER // define this to enable weather interactivity or
-                // comment it out if you wish to disable the weather
+#include "font.h"
+#include "images.h"
+ 
 
 // Customize these to match your install
-const char* ssid        = "privat_network";
-const char* password    = "secret";
+const char* ssid        = "private_network";
+const char* password    = "seekret";
 const char* weatherHost = "api.wunderground.com";
 const char* state       = "MO";
 const char* city        = "Blue_Springs";
-const char* api_key     = "getyourownkey";
+const char* api_key     = "getyour0wnkey";
 const float latitude    = 39.02000046;
 const float longitude   = -94.27999878;
 const signed int offset = -6;
@@ -49,15 +65,109 @@ struct SunTimes {
 
 struct Weather {
   const char* observation;
+  float temp_f;
 };
 
 // Globals 
 TimeElements sunrise; // These are LOCAL time not GMT
 TimeElements sunset;  // because they are build by adding an offset to the current date/time
+float last_temp;      // save this so I don't eat my API calls 
+const char * last_observation;
+
+// Water temp reading
+
+#define ONE_WIRE_BUS 2 // Data wire is plugged into GPIO2 on the ESP
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+// arrays to hold device address
+DeviceAddress waterThermometer;
+
+SSD1306  display(0x3c, D3, D5);
+OLEDDisplayUi ui     ( &display );
 
 static char respBuf[4096];
 
-IRsend irsend(0); // init a sender
+IRsend irsend(4); // init a sender on GPIO4
+
+#define AIR_TEMP_PIN 0 // TMP 36 is on analog 0
+
+String twoDigits(int digits){
+  if(digits < 10) {
+    String i = '0'+String(digits);
+    return i;
+  }
+  else {
+    return String(digits);
+  }
+}
+
+void clockOverlay(OLEDDisplay *display, OLEDDisplayUiState* state) {
+  display->setTextAlignment(TEXT_ALIGN_RIGHT);
+  display->setFont(ArialMT_Plain_10);
+  String text;
+  text += String(hourFormat12());
+  text += ":";
+  text += twoDigits(minute());
+  text += ":";
+  text += twoDigits(second());
+  text += " ";
+  if (isAM()) text += "AM  ";
+  else text += "PM  ";
+  text += monthShortStr(month());
+  text += " ";
+  text += day();
+  text += " ";
+  text += year();
+  display->drawString(128, 0, text);
+}
+
+void drawFrame1(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+  // Draw the water temperature icon 
+  display->setTextAlignment(TEXT_ALIGN_LEFT);
+  display->setFont(Open_Sans_12);
+
+  float tempC = sensors.getTempC(waterThermometer);
+  String text;
+  text += "Water Temp: ";
+  text += DallasTemperature::toFahrenheit(tempC);
+  display->drawString(0,20, text);
+  
+}
+
+void drawFrame2(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+  // Draw the outside temperature icon
+  display->setTextAlignment(TEXT_ALIGN_LEFT);
+  display->setFont(Open_Sans_12);
+
+  String text;
+  text += "Outside Temp: ";
+  text += last_temp;
+  text += "\n";
+  text += last_observation;
+  display->drawString(0,20, text);
+}
+
+void drawFrame3(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+  // Draw the inside temperature icon
+  display->setTextAlignment(TEXT_ALIGN_LEFT);
+  display->setFont(Open_Sans_12);
+
+  String text;
+  text += "Inside Temp: ";
+  text += getAirTemp();
+  display->drawString(0,20, text);
+}
+
+FrameCallback frames[] = { drawFrame1, drawFrame2, drawFrame3 };
+
+// how many frames are there?
+int frameCount = 3;
+
+// Overlays are statically drawn on top of a frame eg. a clock
+OverlayCallback overlays[] = { clockOverlay };
+int overlaysCount = 1;
  
 void setup()
 {
@@ -84,7 +194,7 @@ void setup()
         Serial.print(F("Current unix time (GMT): ")); Serial.println(unixTime);
         setTime(unixTime); // set the time in the time library to the real time (in GMT)
         if (!is_dst) adjustTime(offset * 60 * 60); // adjust time to the local time so from now on all time refs are local
-        else adjustTime((offset+1) *60 *60);
+        else adjustTime((offset+1) *60 *60); // this probably breaks if you are +GMT. sorry.
         Serial.print(F("Adjusted unix time (local): ")); Serial.println(now());
 
         MorningAlarm(); // run once for setup of sunrise/sunset alarms the first time the app runs
@@ -92,13 +202,60 @@ void setup()
                         // on first run.  
         
         Alarm.alarmRepeat(5,0,0, MorningAlarm); // at 5:00AM update sunrise/sunset times
-        #ifdef WEATHER
         Alarm.timerRepeat(900, quarterAlarm); // every 15 minute tasks - currently all weather related
-        #endif
+        quarterAlarm(); // again run once to throw weather effects in place right away on device startup
+ 
+        // Set up OTA firmware updates.
+        
+        ArduinoOTA.onStart([]() {
+          Serial.println("Start OTA download");
+        });
+        ArduinoOTA.onEnd([]() {
+          Serial.println("\nEnd OTA download");
+        });
+        ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+          Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+        });
+        ArduinoOTA.onError([](ota_error_t error) {
+          Serial.printf("Error[%u]: ", error);
+            if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+            else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+            else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+            else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+            else if (error == OTA_END_ERROR) Serial.println("End Failed");
+        });
+        ArduinoOTA.begin();
+
+        Serial.print("Locating devices...");
+        sensors.begin();
+        Serial.print("Found ");
+        Serial.print(sensors.getDeviceCount(), DEC);
+        Serial.println(" devices.");
+        if (!sensors.getAddress(waterThermometer, 0)) Serial.println("Unable to find address for Device 0"); 
+        sensors.setResolution(waterThermometer, 9);
+
+        ui.setTargetFPS(60);
+        ui.setActiveSymbol(activeSymbol);
+        ui.setInactiveSymbol(inactiveSymbol);
+        ui.setIndicatorPosition(BOTTOM);
+        ui.setIndicatorDirection(LEFT_RIGHT);
+        ui.setFrameAnimation(SLIDE_LEFT);
+        ui.setOverlays(overlays, overlaysCount);
+        ui.setFrames(frames, frameCount);
+        ui.init();
+
+        display.flipScreenVertically();
+          
 }
 
 void loop() {
-        Alarm.delay(1000);
+        int remainingTimeBudget = ui.update();
+      
+        if (remainingTimeBudget > 0) {
+          sensors.requestTemperatures();
+          Alarm.delay(remainingTimeBudget);
+        }
+        ArduinoOTA.handle();
         yield();
 }
 
@@ -106,19 +263,34 @@ void quarterAlarm() { // every 15 minute task - check the weather and change the
         Weather weather;
         getWeather(weather);
 
+        SunTimes times;
+        getSunrise(times); 
+
         // TODO:
-        // Item 1 - make sure it doesn't change the weather lighting after dusk (dusk trumps weather)
-        // Item 2 - figure out what the other observations are and map them.
+        //  figure out what the other observations are and map them.
 
         Serial.println(weather.observation);
-        if (weather.observation == "Partly Cloudy") 
-            irsend.sendNEC(btn_partlycloudy, 32);
-        
-        if (weather.observation == "Mostly Cloudy")
-            irsend.sendNEC(btn_mostlycloudy, 32);
-         
-        if (weather.observation == "Clear")
-            irsend.sendNEC(btn_blue, 32); // this is probably a bad choice, fix TODO
+        last_temp = weather.temp_f;
+        last_observation = weather.observation;
+
+        if (makeTime(times.sunrise) < now() && makeTime(times.sunset) > now()) // only change weather effects between sunrise and sunset
+        {
+          Serial.println("In weather update loop, It's during the day.");
+          if (weather.observation == "Partly Cloudy") 
+              irsend.sendNEC(btn_partlycloudy, 32);
+          
+          if (weather.observation == "Mostly Cloudy")
+              irsend.sendNEC(btn_mostlycloudy, 32);
+
+          if (weather.observation == "Overcast")
+              irsend.sendNEC(btn_cloudy, 32);
+           
+          if (weather.observation == "Clear")
+              irsend.sendNEC(btn_blue, 32); 
+              
+        } else {
+          Serial.println("Not updating weather, it's dark.");
+        }
 }
 
 void MorningAlarm() {
@@ -137,10 +309,6 @@ void MorningAlarm() {
         Alarm.triggerOnce(makeTime(times.sunrise) + 3600, alarmDay);  // full light
         Alarm.triggerOnce(makeTime(times.sunset), alarmDusk);        // dusk button
         Alarm.triggerOnce(makeTime(times.sunset) + 3600, alarmNight);   // night button
-
-        // compare current time to sunrise/sunset times and set the lights as appropriate.
-        // this will fix the first run situation. on subsequent runs it will do nothing as the lights
-        // will already be correct.
 }
 
 void getSunrise(SunTimes& times) {
@@ -259,7 +427,30 @@ void getWeather(Weather& weather) {
         int response_features_conditions = response["features"]["conditions"]; // 1
         JsonObject& current_observation = root["current_observation"];
         weather.observation = current_observation["weather"]; // "Partly Cloudy"
+        weather.temp_f = current_observation["temp_f"]; // 66.3
 
+}
+
+float getAirTemp() {
+   int reading = analogRead(AIR_TEMP_PIN);  
+   
+   // converting that reading to voltage, for 3.3v arduino use 3.3
+   float voltage = reading * 3.3;
+   voltage /= 1024.0; 
+   
+   // print out the voltage
+   Serial.print(voltage); Serial.println(" volts");
+   
+   // now print out the temperature
+   float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
+                                                 //to degrees ((voltage - 500mV) times 100)
+   Serial.print(temperatureC); Serial.println(" degrees C");
+   
+   // now convert to Fahrenheit
+   float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+   Serial.print(temperatureF); Serial.println(" degrees F");
+
+   return temperatureF;
 }
 
 void alarmSunrise() {
@@ -281,6 +472,11 @@ void alarmNight() {
   Serial.println(F("It's nighttime"));
   irsend.sendNEC(btn_night, 32);
 }
+
+
+
+
+
 
 /*
  * © Francesco Potortì 2013 - GPLv3
